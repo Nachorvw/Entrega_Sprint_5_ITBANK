3d70bd7cf58ac9511d45df774e3834a395232c59
Nachorvw <nachorewe@gmail.com>
2022-07-17 16:05:08 -0300

    arreglos en el lector

--- Lector_json.py
+++ Lector_json.py
@@ -12,29 +12,29 @@ tipo_cliente = ""
 direccion_cliente = []
 
 with open("eventos_classic.json") as classic:
-    data_json = classic.read()
-    data = json.loads(data_json)
-# print(data)
-for transaccion in data["transacciones"]:
+    data_json_classic = classic.read()
+    data_classic = json.loads(data_json_classic)
+# print(data_classic)
+for transaccion in data_classic["transacciones"]:
      diccionario_transacciones.append(transaccion)
      
      
      
-for datos in data:
-    nombre_cliente = data['nombre']
-    apellido_cliente = data['apellido']
-    dni_cliente = data['dni']
-    tipo_cliente = data['tipo']
+for datos in data_classic:
+    nombre_cliente = data_classic['nombre']
+    apellido_cliente = data_classic['apellido']
+    dni_cliente = data_classic['dni']
+    tipo_cliente = data_classic['tipo']
     
     
-# print(json.dumps(data, indent=2))
+# print(json.dumps(data_classic, indent=2))
 print(nombre_cliente)
 print(apellido_cliente)
 print(dni_cliente)
 print(tipo_cliente)
-print(data['direccion']['calle'])
-print(data['direccion']['numero'])
-print(data['direccion']['ciudad'])
-print(data['direccion']['pais'])
-print(data['direccion']['provincia'])
+print(data_classic['direccion']['calle'])
+print(data_classic['direccion']['numero'])
+print(data_classic['direccion']['ciudad'])
+print(data_classic['direccion']['pais'])
+print(data_classic['direccion']['provincia'])
 print(diccionario_transacciones[0]['estado'])
